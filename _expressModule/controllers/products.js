1df27606a51dc056d8afc7e409664ab09d1aa8ce
breno-do-val <brenorvale@gmail.com>
2020-03-19 00:31:50 -0300

    Configured the model for product registering and used a file as database

--- _expressModule/controllers/products.js
+++ _expressModule/controllers/products.js
@@ -1,4 +1,4 @@
-const products = []
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res) => {
     res.render('add-product', 
@@ -10,17 +10,20 @@ exports.getAddProduct = (req, res) => {
 }
 
 exports.postProduct = (req, res) => {
-    products.push({ title: req.body.title })
+    const product = new Product(req.body.title)
+    product.save()
     res.redirect('/')
 }
 
 exports.getProducts = (req, res) => {
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        })
     })
 }
