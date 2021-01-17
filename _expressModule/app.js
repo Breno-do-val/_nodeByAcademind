0e0b99b5df795c2ac6e424acbb728952ce7bab94
breno-do-val <brenorvale@gmail.com>
2021-01-17 10:48:27 -0300

    (feat): add user Sequelize Model

--- _expressModule/app.js
+++ _expressModule/app.js
@@ -12,23 +12,56 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// A middleware is registered and only executed for incoming requests
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch()
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    //console.log(result)
-}).catch(err => {
-    console.error(err)
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Product);
+
+sequelize.sync()
+    .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({
+                name: 'Breno',
+                email: 'teste@teste.com.br'
+            });
+        }
+        return Promise.resolve(user);
+    })
+    .then(_ => {
+        app.listen(port, () => {
+            console.log(`App is running on Port ${port}`)
+        });
+    })
+    .catch(err => {
+        console.error(err)
 })
 
-app.listen(port, () => {
-    console.log(`App is running on Port ${port}`)
-});
