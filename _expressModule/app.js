122d6dc76a30e83ce6847bc95a505fdbc842d4b1
breno-do-val <brenorvale@gmail.com>
2021-01-24 22:22:19 -0300

    !(refact): refactor add to cart

--- _expressModule/app.js
+++ _expressModule/app.js
@@ -50,7 +50,7 @@ Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
-sequelize.sync({ force: true})
+sequelize.sync()
     .then(result => {
         return User.findByPk(1);
     })
--- _expressModule/controllers/shop.js
+++ _expressModule/controllers/shop.js
@@ -61,6 +61,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQty = 1;
   req.user
     .getCart()
     .then(cart => {
@@ -72,15 +73,17 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQty = 1;
       if (product) {
-        // ...
+        const oldQuantity = product.cartItem.quantity;
+        newQty = oldQuantity + 1;
+        return product;
       }
       return Product.findByPk(prodId)
-        .then(product => {
-          return fetchedCart.addProduct(product, { through: { quantity: newQty}});
-        })
-        .catch(err => console.log(err));
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQty}
+      });
     })
     .then(() => {
       res.redirect('/cart');
